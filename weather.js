85ba23deaf8c7a2af3217c3f3cf21993cbfbe2da
Phil Fevry <phil.fevry@gmail.com>
2017-05-16 05:18:54 -0400

    modified script

--- weather.js
+++ weather.js
@@ -12,18 +12,22 @@
  *  Copyright 2017. All Rights Reserved.
  *
  *	TODO:
- *		- Write code to modify DOM objects
+ *		- JSON interpretor
+ *		- Temperature conversion functions
  *		- Reorganize and clean up code.
- *		- Obtain SSL Certificate to ensure crossbrowser compatibility
+ *		- Obtain SSL Certificate to ensure crossbrowser compatibility with geolocation
  *		  with navigator.geolocation.
  *		- Register for; and, apply  DarkSky API.
  *		- Integrate into Portfolio Website
  *  
  */
 
-API_URL = "https://api.darksky.net/forecast/";
+API_URL = 'https://api.darksky.net/forecast/';
 API_KEY = '';
+
 APP_VERSION = 0.01;
+
+HOME_URL = 'http://www.phlfvry.com/';
 
 FAHRENHEIT = '&#8457;';
 CELSIUS = '&#8451;';
@@ -33,42 +37,64 @@ $(document).ready(function() {
 });
 
 function weatherApp() {
-	update('label','Version', 'Version: ' + APP_VERSION);
-	document.getElementById('btn_MetricToggle').addEventListener('click', toggleTemperatureMetric);
+	// initializers
+	document.title += ' (v.' + APP_VERSION + ')';
+	set('label','Version',  APP_VERSION);
+	
+	var link = get('link', 'Homepage');
+	link.href = HOME_URL;
+	
+	get('btn','MetricToggle').addEventListener('click', toggleTemperatureMetric);
 
 	promptUserForLocation();
 }
 
 function promptUserForLocation() {
-	 try {
-	 	
+	 try {	 	
 	 	var navObject = navigator.geolocation;
 	 	if (!navObject) throw 'Unsupported Browser';
 	 	
-		navObject.getCurrentPosition(callAPIWithLocation);
-	 	 
+		navObject.getCurrentPosition(getJSONFromAPI);
 	} catch(e) {
 		console.log('Error: ' + e)	
 	}
 }
 
 function toggleTemperatureMetric() {
-	update('label', 'TempMetric', CELSIUS);
+	var label = get('label','TempMetric');
+	
+	if (label.innerHTML == '℉') { // change this
+		// convert label_Temperature to celsius
+		set('label', 'TempMetric', CELSIUS);
+	} else {
+		// convert label_Temperature to fahrenheit
+		set('label', 'TempMetric', FAHRENHEIT);
+	}
 }
 
-function update(element, id, newValue) {
-	var object = document.getElementById(element + '_' + id);
+function get(type, id) {
+	return document.getElementById(type + '_' + id);
+}
+
+function set(type, id, newValue) {
+	// '*' type updates actual element tag
+	if (type == '*') {
+		var object = document.getElementsByTagName(id);	
+	} else {
+		var object = get(type, id);
+	}
 	object.innerHTML = newValue;
 }
 
-function callAPIWithLocation(location) {
+function getJSONFromAPI(location) {
 	var latitude = location.coords.latitude;
 	var longitude = location.coords.longitude;
 	
 	var api = API_URL + API_KEY +  '/' + latitude + ',' + longitude;
-    
+	
+	set('label','UserLocation', '{' + latitude + ',' + longitude + '}');
 	// call API
-	// get response from API
-	// update page elements with results from API
+	// get JSON from API
+	// update page elements
 }
 
