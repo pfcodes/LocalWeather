422b1b65b7050a4693927bf6e4a179292f60dc34
Phil Fevry <phil.fevry@gmail.com>
2017-05-22 21:00:46 -0400

    refactored code, began prototyping redesign

--- weather.js
+++ weather.js
@@ -1,122 +1,125 @@
 /*
+ *  by phlfvry
  *  Copyright 2017. All Rights Reserved.
  *  
  */
+var weatherApp = {
+	constants: function() {	return {
+		APP_VERSION: '1.5',
+		HOME_URL: 'http://www.phlfvry.com/',
 
-function CONST(key) {
-	var dict = {
-		'APP_VERSION': '1.0',
-		'HOME_URL': 'http://www.phlfvry.com/',
-
-		'WEATHER_API_BASEURL': 'https://api.darksky.net/forecast/',
-		'WEATHER_API_KEY': '165abf3d9f0478fd6a5d0d053a8e52c8',
-		'WEATHER_API_FULLURL': this['WEATHER_API_BASEURL'] + this['WEATHER_API_KEY'],
+		WEATHER_API_BASEURL: 
+		'https://api.darksky.net/forecast/165abf3d9f0478fd6a5d0d053a8e52c8/',
 		
-		'GOOGLEMAPS_API_BASEURL': 'https://maps.googleapis.com/maps/api/geocode/json?',
+		GOOGLEMAPS_API_BASEURL: 
+		'https://maps.googleapis.com/maps/api/geocode/json?latlng=',
 		
-		'LABEL_WELCOMEMESSAGE': 'Please allow location access',
-		'LABEL_APP_HEADER': 'Weather',
-		'LABEL_TOGGLE_BTN': 'Switch Units',
-		'LABEL_FAHRENHEIT': '&#176;F',
-		'LABEL_CELSIUS': '&#176;C',
-		'LABEL_HOME_URL': '&lt; pf / &gt;'
-	};
+		LABEL_WELCOMEMESSAGE: 'Please allow location access',
+		LABEL_APP_HEADER: 'Weather',
+		LABEL_TOGGLE_BTN: 'Switch Units',
+		LABEL_FAHRENHEIT: '&#176;F',
+		LABEL_CELSIUS: '&#176;C',
+		LABEL_HOME_URL: '&lt; pf / &gt;',
 
-	return dict[key];
-}
+		LABEL_WELCOME_MESSAGE: 'Please allow location access.'
+		};
+	}(),
 
-$(document).ready(function() {
-	weatherApp();
-});
+	init: function() {
+		set('label','HeaderTitle', this.constants.LABEL_APP_HEADER);
+		set('label','Version', 'version ' + this.constants.APP_VERSION);
+		
+		var linkToHome = get('link', 'Homepage');
+		linkToHome.href = this.constants.HOME_URL;
+		linkToHome.innerHTML = this.constants.LABEL_HOME_URL;
+		
+		var toggleButton = get('btn', 'MetricToggle');
+		toggleButton.innerHTML = this.constants.LABEL_TOGGLE_BTN;
+		toggleButton.addEventListener('click', this.toggleTemperatureMetric);
 
-function weatherApp() {
-	console.log(CONST('LABEL_APP_HEADER'));
+		set('label','WelcomeMessage', this.constants.LABEL_WELCOME_MESSAGE);
+	},
 
-	set('label','HeaderTitle', CONST('LABEL_APP_HEADER'));
-	set('label','Version', 'version ' + CONST('APP_VERSION'));
-	
-	var linkToHome = get('link', 'Homepage');
-	linkToHome.href = CONST('HOME_URL');
-	linkToHome.innerHTML = CONST('LABEL_HOME_URL');
-	
-	var toggleButton = get('btn', 'MetricToggle');
-	toggleButton.innerHTML = CONST('LABEL_TOGGLE_BTN');
-	toggleButton.addEventListener('click', toggleTemperatureMetric);
+	start: function() {
+		try {
+			if (!navigator.geolocation) throw 'Unsupported Browser';
+			navigator.geolocation.getCurrentPosition(this.getLocationBasedData);
+		} catch (e) {
+			set('label', 'WelcomeMessage', 'Unsupported Browser');
+			console.log('Error: ' + e);
+		}
+	},
 
-	set('label','WelcomeMessage', CONST('LABEL_WELCOME_MESSAGE'));
+	getLocationBasedData: function(location) {
+		set('label', 'WelcomeMessage', 'Loading...');
+		var latit = location.coords.latitude;
+		var longit = location.coords.longitude;
+		var locationapi = weatherApp.constants.GOOGLEMAPS_API_BASEURL;
+		var weatherapi = weatherApp.constants.WEATHER_API_BASEURL;
+		locationapi += latit+','+longit;
+		weatherapi += latit+','+longit;
+		weatherApp.updateLocation(locationapi);
+		weatherApp.updateWeather(weatherapi);
+	},
 
-	getUserLocation();
-}
-
-function getUserLocation() {
-	 try {	 	
-	 	var navObject = navigator.geolocation; 	
-	 	if (!navObject) throw 'Unsupported Browser';		
- 		navObject.getCurrentPosition(getJSONFromAPI); // call getJSONFromAPI() with location info
-	} catch(e) {	
-		set(label, 'WelcomeMessage',
-			'Error: Browser is not supported. Try another one.'
-		);
-		console.log('Error: ' + e)	
-	}
-}
-
-function getJSONFromAPI(location) {
-	var latitude = location.coords.latitude;
-	var longitude = location.coords.longitude;	
-	var weatherapi = CONST('WEATHER_API_BASEURL') + CONST('WEATHER_API_KEY') + '/' + latitude + ',' + longitude;
- 	var googlemapsapi = CONST('GOOGLEMAPS_API_BASEURL') + 'latlng='+latitude+','+longitude;
-
-	set('label', 'WelcomeMessage', 'Loading...');
-
-	// GoogleMaps API for location information
-	var cityandstate = '';
-	var googlemapsxhr = new XMLHttpRequest();
-	googlemapsxhr.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) { // succesful request GoogleMaps API
+	updateLocation: function(param) {
+		console.log('Update Location: ' + param);
+		var cityandstate = '';
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+	 		if (this.readyState == 4 && this.status == 200) {	
 			data = JSON.parse(this.responseText);
 			cityname = data.results[0].address_components[2].short_name;
 			statename = data.results[0].address_components[4].short_name;
 			cityandstate = cityname + ', ' + statename;
-			setLocation(cityandstate); // assign value to DOM
-		}
-	}
-	googlemapsxhr.open("GET", googlemapsapi, true);
-	googlemapsxhr.send();
+			setLocation(cityandstate);
+			}
+		};
+		xhr.open('GET', param, true);
+		xhr.send();
 
-	// call Weather API for weather information
-	var weatherxhr = new XMLHttpRequest();
-	weatherxhr.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) { // succesful request from Weather API
+	},
+
+	updateWeather: function(param) {
+		console.log('Update Weather: ' + param);
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function () {	
+			if (this.readyState == 4 && this.status == 200) {
 			remove('label', 'WelcomeMessage');
-			data = JSON.parse(this.responseText);
+			var data = JSON.parse(this.responseText);
 			setTemperature(Math.round(data.currently.temperature));
 			show('btn', 'MetricToggle');
-			set('label','TempMetric', CONST('LABEL_FAHRENHEIT'));
+			set('label', 'TempMetric', weatherApp.constants.LABEL_FAHRENHEIT);
+			}
 		}
-	}
-	weatherxhr.open("GET", weatherapi, true);
-	weatherxhr.send();
-}
+		xhr.open('GET', param, true);
+		xhr.send();
+	},
 
-function toggleTemperatureMetric() {
-	var label = get('label','TempMetric');
-	if (label.innerHTML == '°F') {
-		// convert to celsius
-		var oldValue = get('label','Temperature').innerHTML;
-		var newValue = Math.round((oldValue - 32) * 0.5556);
-		set('label', 'Temperature', newValue);
-		set('label', 'TempMetric', CONST('LABEL_CELSIUS'));
-	} else {
-		// convert to fahrenheit
-		var oldValue = get('label','Temperature').innerHTML;
-		var newValue = Math.round((oldValue * 1.8) + 32);
-		set('label', 'Temperature', newValue);
-		set('label', 'TempMetric', CONST('LABEL_FAHRENHEIT'));
-	}
-}
+	toggleTemperatureMetric: function() {
+		var label = get('label', 'TempMetric');
+		var o = get('label', 'Temperature').innerHTML;
+		var n = 0;
 
-// Convenience functions
+		if (label.innerHTML == '°F') {
+			n = Math.round((o - 32) * 0.5556);
+			set('label', 'TempMetric', weatherApp.constants.LABEL_CELSIUS);
+		} else {
+			// convert to fahrenheit
+			n = Math.round((o * 1.8) + 32);
+			set('label', 'TempMetric', weatherApp.constants.LABEL_FAHRENHEIT);
+		}
+
+		setTemperature(n);
+	}
+};
+
+$(document).ready(function() {
+	weatherApp.init();
+	weatherApp.start();
+});
+
+// Convenience functions -- add to a namespace
 function get(type, id) { return document.getElementById(type + '_' + id); }
 function set(type, id, newValue) { get(type, id).innerHTML = newValue; }
 function show(type, id)	{ get(type, id).style.visibility = 'visible'; }
