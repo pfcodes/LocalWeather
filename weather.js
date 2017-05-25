93b70d6a92f24709469f986764501ef83ce6ee67
Phil Fevry <phil.fevry@gmail.com>
2017-05-25 16:07:32 -0400

    more refactoring

--- weather.js
+++ weather.js
@@ -1,23 +1,22 @@
- var weatherApp, s;
+ var WeatherApp, GeoLocation, s;
 
-weatherApp = {
+WeatherApp = {
 
 	properties: {
-		title: $('#label_AppTitle'),
-		subtitle: $('#label_AppSubtitle'),
-		version: $('#label_Version'),
-		welcome: $('#label_WelcomeMessage'),
-		temperature: $('#label_Temperature'),
-		toggleButton: $('#btn_MetricToggle'),
-		homeAnchor: $('#link_Homepage'),
-
+		
 		api: {
 			weather: 'https://api.darksky.net/forecast/165abf3d9f0478fd6a5d0d053a8e52c8/',
 			maps: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=',
 		}, 
 
-		LABEL_FAHRENHEIT: '&#176;F',
-		LABEL_CELSIUS: '&#176;C',
+		title: $('#label_AppTitle'),
+		subtitle: $('#label_AppSubtitle'),
+		version: $('#label_Version'),
+		welcome: $('#label_WelcomeMessage'),
+		temperature: $('#label_Temperature'),
+		temperatureLabel: $('#label_TempMetric'),
+		toggleButton: $('#btn_MetricToggle'),
+		homeAnchor: $('#link_Homepage'),
 	},
 
 	init: function() {
@@ -38,7 +37,7 @@ weatherApp = {
 			if (!navigator.geolocation) throw 'Unsupported Browser';
 			navigator.geolocation.getCurrentPosition(this.getLocationBasedData);
 		} catch (e) {
-			s.welcome.text('Unsupported Browser');
+			s.welcome.text('Error: ' + e);
 			console.log('Error: ' + e);
 		}
 	},
@@ -46,16 +45,14 @@ weatherApp = {
 	getLocationBasedData: function(location) {
 		s.welcome.text('Loading...');
 
-		var api = weatherApp.properties.api;
 		var apiQuery = location.coords.latitude+','+location.coords.longitude;
 
-		weatherApp.updateLocation(api.maps + apiQuery);
-		weatherApp.updateWeather(api.weather + apiQuery);
+		WeatherApp.updateLocation(s.api.maps + apiQuery);
+		WeatherApp.updateWeather(s.api.weather + apiQuery);
 	},
 
 	updateLocation: function(param) {
 		console.log('Update Location: ' + param);
-		var cityandstate = '';
 		var xhr = new XMLHttpRequest();
 		xhr.onreadystatechange = function() {
 	 		if (this.readyState == 4 && this.status == 200) {	
@@ -79,7 +76,7 @@ weatherApp = {
 			s.subtitle.text(data.currently.summary);
 			s.temperature.text(Math.round(data.currently.temperature));
 			s.toggleButton.css('visibility', 'visible');
-			set('label', 'TempMetric', weatherApp.properties.LABEL_FAHRENHEIT);
+			s.temperatureLabel.html('&deg;F');
 			}
 		}
 		xhr.open('GET', param, true);
@@ -87,30 +84,23 @@ weatherApp = {
 	},
 
 	toggleTemperatureMetric: function() {
-		var label = get('label', 'TempMetric');
-		var o = get('label', 'Temperature').innerHTML;
-		var n;
-
-		if (label.innerHTML == '°F') {
-			n = Math.round((o - 32) * 0.5556);
-			set('label', 'TempMetric', weatherApp.properties.LABEL_CELSIUS);
+		var newTemperature;
+		var currentTemperature = s.temperature.text();
+		var label = s.temperatureLabel;
+		if (label.text() == '°F') {
+			// converts to celsius
+			newTemperature = Math.round((currentTemperature - 32) * 0.5556);
+			label.html('&deg;C');
 		} else {
-			// convert to fahrenheit
-			n = Math.round((o * 1.8) + 32);
-			set('label', 'TempMetric', weatherApp.properties.LABEL_FAHRENHEIT);
+			// converts to fahrenheit
+			newTemperature = Math.round((currentTemperature * 1.8) + 32);
+			label.html('&deg;F');
 		}
-		s.temperature.text(n);
+		s.temperature.text(newTemperature);
 	}
 };
 
 $(function() {
-	weatherApp.init();
+	WeatherApp.init();
 });
-
-// Convenience functions -- add to a namespace
-function get(type, id) { return document.getElementById(type + '_' + id); }
-function set(type, id, newValue) { get(type, id).innerHTML = newValue; }
-function show(type, id)	{ get(type, id).style.visibility = 'visible'; }
-function hide(type, id) { get(type, id).style.visibility = 'hidden'; }
-function remove(type, id) { get(type, id).remove(); }
 
