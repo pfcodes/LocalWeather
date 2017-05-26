a42e6f47fcad63ece5fed07736b32b14c58c3581
Phil Fevry <phil.fevry@gmail.com>
2017-05-26 17:15:29 -0400

    refactored code with more jquery

--- weather.js
+++ weather.js
@@ -1,14 +1,13 @@
- var WeatherApp, GeoLocation, s;
+
+var WeatherApp, CanvasArtisan, s;
 
 WeatherApp = {
 
 	properties: {
-		
 		api: {
 			weather: 'https://api.darksky.net/forecast/165abf3d9f0478fd6a5d0d053a8e52c8/',
 			maps: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=',
 		}, 
-
 		title: $('#label_AppTitle'),
 		subtitle: $('#label_AppSubtitle'),
 		version: $('#label_Version'),
@@ -32,7 +31,6 @@ WeatherApp = {
 		s.welcome.text('Please allow location access.');
 		s.toggleButton.text('Switch Units');
 		s.toggleButton.on('click', this.toggleTemperatureMetric);
-
 		try {
 			if (!navigator.geolocation) throw 'Unsupported Browser';
 			navigator.geolocation.getCurrentPosition(this.getLocationBasedData);
@@ -44,48 +42,37 @@ WeatherApp = {
 
 	getLocationBasedData: function(location) {
 		s.welcome.text('Loading...');
-
 		var apiQuery = location.coords.latitude+','+location.coords.longitude;
-
 		WeatherApp.updateLocation(s.api.maps + apiQuery);
 		WeatherApp.updateWeather(s.api.weather + apiQuery);
 	},
 
 	updateLocation: function(param) {
 		console.log('Update Location: ' + param);
-		var xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = function() {
-	 		if (this.readyState == 4 && this.status == 200) {	
-			data = JSON.parse(this.responseText);
-			cityname = data.results[0].address_components[2].short_name;
-			statename = data.results[0].address_components[4].short_name;
-			s.title.text(cityname + ' ' + statename);
-			}
-		};
-		xhr.open('GET', param, true);
-		xhr.send();
+		$.getJSON(param, function(data) {
+			s.title.text(
+				data.results[0].address_components[2].short_name+' '+
+				data.results[0].address_components[4].short_name
+			);
+			$(document).attr('title', data.results[0].address_components[2].short_name +
+				' - Weather Conditions ');
+		});
 	},
 
 	updateWeather: function(param) {
 		console.log('Update Weather: ' + param);
-		var xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = function () {	
-			if (this.readyState == 4 && this.status == 200) {
+		$.getJSON(param, function(data) {
 			s.welcome.remove();
-			var data = JSON.parse(this.responseText);
 			s.subtitle.text(data.currently.summary);
 			s.temperature.text(Math.round(data.currently.temperature));
 			s.toggleButton.css('visibility', 'visible');
 			s.temperatureLabel.html('&deg;F');
-			}
-		}
-		xhr.open('GET', param, true);
-		xhr.send();
+		});
 	},
 
 	toggleTemperatureMetric: function() {
-		var newTemperature;
-		var currentTemperature = s.temperature.text();
+		var newTemperature, currentTemperature;
+		currentTemperature = s.temperature.text();
 		var label = s.temperatureLabel;
 		if (label.text() == '°F') {
 			// converts to celsius
@@ -99,6 +86,8 @@ WeatherApp = {
 		s.temperature.text(newTemperature);
 	}
 };
+
+var CanvasArtisan = {};
 
 $(function() {
 	WeatherApp.init();
